bc4c5d3fe10f07f2a658b46a017a14fc41bccd2f
Dale Hille <1749799+dalehille@users.noreply.github.com>
2023-12-05 05:09:07 -0500

    fzf updates

--- khist.sh
+++ khist.sh
@@ -15,34 +15,35 @@
 DB_PATH="$HOME/.kwrapper/$(kubectl config current-context)_kwrapper.db"
 
 # morhetz/gruvbox
-export FZF_DEFAULT_OPTS='--color=bg+:#3c3836,bg:#32302f,spinner:#fb4934,hl:#928374,fg:#ebdbb2,header:#928374,info:#8ec07c,pointer:#fb4934,marker:#fb4934,fg+:#ebdbb2,prompt:#fb4934,hl+:#fb4934'
+# export FZF_DEFAULT_OPTS='--color=bg+:#3c3836,bg:#32302f,spinner:#fb4934,hl:#928374,fg:#ebdbb2,header:#928374,info:#8ec07c,pointer:#fb4934,marker:#fb4934,fg+:#ebdbb2,prompt:#fb4934,hl+:#fb4934'
+# export FZF_DEFAULT_OPTS='--color=bg+:#4c4846,bg:#423c3a,spinner:#fc6955,hl:#a29884,fg:#fcf6c2,header:#a29884,info:#9edc7d,pointer:#fc6955,marker:#fc6955,fg+:#fcf6c2,prompt:#fc6955,hl+:#fc6955'
+# export FZF_DEFAULT_OPTS='--color=bg+:#4c4846,bg:#423c3a,spinner:#fc6955,hl:#a29884,fg:#fcf6c2,header:#a29884,info:#9edc7d,pointer:#fc6955,marker:#fc6955,fg+:#000000,prompt:#fc6955,hl+:#ffff00'
+# export FZF_DEFAULT_OPTS='--color=bg+:#4c4846,bg:#423c3a,spinner:#fc6955,hl:#a29884,fg:#fcf6c2,header:#a29884,info:#9edc7d,pointer:#fc6955,marker:#fc6955,fg+:#ffffff,prompt:#fc6955,hl+:#0000ff'
+# export FZF_DEFAULT_OPTS='--color=bg+:#4c4846,bg:#423c3a,spinner:#fc6955,hl:#a29884,fg:#fcf6c2,header:#a29884,info:#9edc7d,pointer:#fc6955,marker:#fc6955,fg+:#98FB98,prompt:#fc6955,hl+:#0000ff'
+# export FZF_DEFAULT_OPTS='--color=bg+:#4c4846,bg:#423c3a,spinner:#fc6955,hl:#a29884,fg:#fcf6c2,header:#a29884,info:#9edc7d,pointer:#32CD32,marker:#fc6955,fg+:#98FB98,prompt:#fc6955,hl+:#0000ff'
+# export FZF_DEFAULT_OPTS='--color=bg+:#4c4846,bg:#423c3a,spinner:#fc6955,hl:#a29884,fg:#fcf6c2,header:#a29884,info:#9edc7d,pointer:#32CD32,marker:#32CD32,fg+:#98FB98,prompt:#fc6955,hl+:#0000ff'
+export FZF_DEFAULT_OPTS='--color=bg+:#4c4846,bg:#423c3a,spinner:#fc6955,hl:#a29884,fg:#fcf6c2,header:#a29884,info:#9edc7d,pointer:#32CD32,marker:#32CD32,fg+:#98FB98,prompt:#fc6955,hl+:#32CD32'
 
 # Use sqlite3 to get a list of all kubectl commands run in the past
 # This assumes you have a table named 'kwrapper' with a 'command' column
-# COMMANDS=$(sqlite3 $DB_PATH "SELECT timestamp, command FROM kwrapper")
-COMMANDS=$(sqlite3 $DB_PATH "SELECT timestamp,  command FROM kwrapper")
-# echo "$COMMANDS"
-# works - SELECTED_COMMAND=$(echo "$COMMANDS" | fzf --reverse --preview "echo '{}' | awk '{print $1}' | xargs -I CMD sqlite3 $DB_PATH \"SELECT output FROM kwrapper WHERE command = '\"CMD\"'\" | base64 -D | less -R")
-# SELECTED_COMMAND=$(echo "$COMMANDS" | fzf --reverse --preview "echo '{}' | cut -d'|' -f2- | xargs -I CMD sqlite3 $DB_PATH \"SELECT output FROM kwrapper WHERE output = '\"CMD\"'\" | base64 -D | less -R")
-
-
-# SELECTED=$(echo "$COMMANDS" | fzf --reverse --preview "echo '{}'")
-# SELECTED_COMMAND=$(echo "$SELECTED" | cut -d'|' -f2-)
-# OUTPUT=$(sqlite3 $DB_PATH "SELECT output FROM kwrapper WHERE command = '$SELECTED_COMMAND'" | base64 -D)
-# echo $OUTPUT | less -R
-SELECTED_COMMAND=$(echo "$COMMANDS" | fzf --reverse --preview "echo '{}' | awk -F'|' '{print $1}' | xargs -I CMD sqlite3 $DB_PATH \"SELECT output FROM kwrapper WHERE timestamp = CMD\" | less -R")
-# SELECTED_COMMAND=$(echo "$COMMANDS" |  awk -F'|' '{print $1}' | fzf --reverse --preview "echo '{}' | awk -F'|' '{print $1}' | xargs -I CMD sqlite3 $DB_PATH \"SELECT output FROM kwrapper WHERE timestamp = CMD\" | less -R")
-
-# SELECTED_COMMAND=$(echo "$COMMANDS" | fzf --reverse --preview "echo '{}' | awk -F'|' '{print $1}' ")
-
-# SELECTED_COMMAND=$(echo "$COMMANDS" | fzf --reverse --preview "echo '{}' | cut -d'|' -f1 | less -R")
-
-# Use fzf for interactive filtering of the commands
-# SELECTED_COMMAND=$(echo "$COMMANDS" | fzf --reverse --preview "echo '{}' | base64 --decode | less -R")
-# SELECTED_COMMAND=$(echo "$COMMANDS" | fzf --reverse --preview "echo '{}' | xargs -I CMD sqlite3 $DB_PATH \"SELECT output FROM kwrapper WHERE command = CMD\" | less -R")
-# SELECTED_COMMAND=$(echo "$COMMANDS" | fzf --reverse --preview "echo '{}' | xargs -I CMD printf '%q' CMD | xargs -I ESCAPED_CMD sqlite3 $DB_PATH \"SELECT output FROM kwrapper WHERE command = ESCAPED_CMD\" | less -R")
+COMMANDS=$(sqlite3 $DB_PATH "SELECT id, timestamp, command FROM kwrapper")
+# echo "$COMMANDS | fzf --reverse"
 
 # Display the output of the selected command
 # This assumes you have an 'output' column in the same table
 # OUTPUT=$(sqlite3 $DB_PATH "SELECT output FROM kwrapper WHERE command = '$SELECTED_COMMAND'")
 # echo $OUTPUT
+
+
+# Use fzf to select a command and cut to get the ID
+# SELECTED_COMMAND_ID=$(echo "$COMMANDS" | fzf --reverse | cut -d '|' -f 1)
+SELECTED_COMMAND_ID=$(echo "$COMMANDS" | fzf --reverse --bind 'ctrl-j:down,ctrl-k:up' | cut -d '|' -f 1)
+
+# Fetch the output of the selected command using the ID
+OUTPUT_BASE64=$(sqlite3 $DB_PATH "SELECT output FROM kwrapper WHERE id = '$SELECTED_COMMAND_ID'")
+
+# Decode the base64 output
+OUTPUT=$(echo "$OUTPUT_BASE64" | base64 --decode)
+
+# Display the output
+printf "%b\n" "$OUTPUT"

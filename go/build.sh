ab8c10bb7aa1c698d6e0398cdf6e85e6863aa76f
Dale Hille <1749799+dalehille@users.noreply.github.com>
2023-12-18 09:47:43 -0500

    fix for intel mac

--- go/build.sh
+++ go/build.sh
@@ -4,12 +4,15 @@
 PROGRAM_NAME="khist"
 
 # Mac Intel
-GOOS=darwin GOARCH=amd64 go build -o ${PROGRAM_NAME}_mac_intel main.go
+# GOOS=darwin GOARCH=amd64 go build -o ${PROGRAM_NAME}_mac_intel main.go
+# Mac Intel
+# if building on m1 mac then open terminal using Rosetta 2 and run this
+CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o ${PROGRAM_NAME}_mac_intel main.go
 
 # Mac M1
-GOOS=darwin GOARCH=arm64 go build -o ${PROGRAM_NAME}_mac_m1 main.go
+# GOOS=darwin GOARCH=arm64 go build -o ${PROGRAM_NAME}_mac_m1 main.go
 
 # Linux
-GOOS=linux GOARCH=amd64 go build -o ${PROGRAM_NAME}_linux main.go
+# GOOS=linux GOARCH=amd64 go build -o ${PROGRAM_NAME}_linux main.go
 
 echo "Build complete"
